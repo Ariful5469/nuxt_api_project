c0171f3166ec10ad2bceeb1806b978895c98e19d
Ariful5469 <arifulislam546950@gmail.com>
2025-01-27 15:00:03 +0600

    Update fetch configuration and runtimeConfig

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  runtimeConfig: {
+    public: {
+      apiBaseURL: 'http://localhost:3000/api/customers', // Replace with your backend URL
+    },
+  },
+});
